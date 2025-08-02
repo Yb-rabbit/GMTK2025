8b2fba5735f87b224ee097fb755831bff515096f
Yumihoshi <xiaoyuesun915@gmail.com>
2025-08-02 14:34:31 +0800

    【功能】 物品栏UI
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

---
+++ GMTK25/Assets/Scripts/Yumihoshi/Managers/InventoryManager.cs
@@ -0,0 +1,50 @@
+﻿// *****************************************************************************
+// @author: Yumihoshi
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/08/02 13:28
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using System.Collections.Generic;
+using QFramework;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Yumihoshi.SO.Item;
+using Yumihoshi.SO.Item.Weapon;
+using Yumihoshi.Utility;
+
+namespace Yumihoshi.Managers
+{
+    public class
+        InventoryManager : HoshiVerseFramework.Base.Singleton<InventoryManager>
+    {
+        public WeaponData Weapon { get; set; }
+        public BaseItemData ItemInHand { get; set; }
+        public List<BaseItemData> SpareItems { get; } = new();
+
+        public BindableProperty<int> CurActiveSpareItemSize
+        {
+            get;
+            private set;
+        }
+        
+        [LabelText("默认备用物品栏位")] [SerializeField]
+        private int defaultSpareItemSize = 2;
+        [LabelText("备用物品栏位最大值")] [SerializeField]
+        private int maxSpareItemSize = 9;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            CurActiveSpareItemSize =
+                new BindableProperty<int>(defaultSpareItemSize);
+        }
+
+        public void SetCurActiveSpareItemSize(int size)
+        {
+            if (size < defaultSpareItemSize || size > maxSpareItemSize) return;
+            CurActiveSpareItemSize.Value = size;
+        }
+    }
+}
--- GMTK25/Assets/Scripts/Yumihoshi/Test.cs
+++ GMTK25/Assets/Scripts/Yumihoshi/Test.cs
@@ -6,19 +6,28 @@
 // @description:
 // *****************************************************************************
 
+using System.Collections;
 using UnityEngine;
 using Yumihoshi.Entities;
 using Yumihoshi.Managers;
+using Yumihoshi.UI;
 
 namespace Yumihoshi
 {
     public class Test : MonoBehaviour
     {
         public Treasure TreasureTest;
-        
+
         private void Start()
         {
-            Debug.Log(TreasureTest.PickUpTreasure().itemDesc);
+            //Debug.Log(TreasureTest.PickUpTreasure().itemDesc);
+            StartCoroutine(Testt());
+        }
+
+        private IEnumerator Testt()
+        {
+            yield return new WaitForSeconds(3f);
+            
         }
     }
 }
