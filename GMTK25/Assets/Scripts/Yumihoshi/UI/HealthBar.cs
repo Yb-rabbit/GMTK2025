bde4b6bc9db8d3f81df82fcf05c02e8c7d1638f0
Yumihoshi <xiaoyuesun915@gmail.com>
2025-08-03 16:11:47 +0800

    【功能】 血条UI
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

---
+++ GMTK25/Assets/Scripts/Yumihoshi/UI/HealthBar.cs
@@ -0,0 +1,34 @@
+﻿// *****************************************************************************
+// @author: Yumihoshi
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/08/03 15:59
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using System;
+using QFramework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Yumihoshi.UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        private Slider _healthBar;
+        private hp _hp;
+
+        private void Start()
+        {
+            _healthBar = GetComponent<Slider>();
+            _hp = transform.parent.parent.GetComponent<hp>();
+            _hp.HP.Register(HandleHpChanged)
+                .UnRegisterWhenGameObjectDestroyed(gameObject);
+        }
+
+        private void HandleHpChanged(float newHp)
+        {
+            _healthBar.value = newHp;
+        }
+    }
+}
